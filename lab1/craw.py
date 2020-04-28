0ac9c84492f4060d352285c311e39a462625e5cb
1172510217 <2584363094@qq.com>
2020-04-28 18:05:34 +0800

    :racehorse:多线程处理网页内容

--- lab1/craw.py
+++ lab1/craw.py
@@ -2,123 +2,136 @@
 # -*- coding: utf-8 -*-
 
 """
-爬取网页，并提取网页标题和正文，并保存附件（这里主要处理图片资源）
+爬取网页，并提取网页标题和正文，并保存附件（这里主要处理图片资源）.
 """
-
-from bs4 import BeautifulSoup
-import json
-import string
-import time
 from urllib import request, parse
+from bs4 import BeautifulSoup
+from threading import Thread, Lock
+from queue import Queue
+import string
+import json
+import time
+
+lock = Lock()
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                         'Chrome/81.0.4044.122 Safari/537.36 Edg/81.0.416.64'}
+start_url, legal_urls = 'https://news.163.com/rank/', ['news.163.com', 'ent.163.com', 'gov.163.com', 'tech.163.com']
+url_queue, craw_res = Queue(), []
+url_num, url_with_file = 0, 0  # 动态变量，随已爬取网页变化
+num, file_url_num = 1000, 100  # 定值，实验要求参数
 
 
-# todo 使用线程安全的Queue实现多线程爬虫
+def get_urls(k=1.2):
+    url2visit = [start_url]  # 将要访问的URL队列
+    for url in url2visit:
+        print('进度：%.2f' % str(float(len(url2visit)) / (num * 1.2) * 100) + '%')
+        try:
+            f = request.urlopen(request.Request(url, headers=headers))
+        except:
+            continue
+        else:
+            bs = BeautifulSoup(f.read(), 'html.parser', from_encoding='gb18030')
+            if len(url2visit) >= k * num:
+                return url2visit
+            for href in filter(None, map(lambda item: item.get('href'), bs.find_all('a'))):  # 获取所有a标签中的href的值
+                if href.startswith('https://') and str(href).split('/')[2] in legal_urls:
+                    href = parse.quote(href, safe=string.printable)  # 中文字符需要处理
+                    if href not in url2visit:
+                        url2visit.append(href)
 
-class Craw:
 
-    def __init__(self, config_path='./input/config.json'):
-        self.headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
-                                      ' Chrome/81.0.4044.122 Safari/537.36 Edg/81.0.416.64'}
-        with open(config_path, encoding='utf-8') as f:
-            config_dic = json.load(f)
-            self.start_url = config_dic['start_url']  # 开始的URL
-            self.legal_urls = config_dic['legal_urls']  # 合法的URL地址
-            self.url_num = config_dic['url_num']  # 要爬取的URL数目
-            self.url_with_file = config_dic['url_with_file']  # 要爬取的URL中带有附件的URL数目
-            self.craw_res_file = config_dic['craw_res_file']  # json格式的爬取结果的输出文件
-            self.url2visit = [self.start_url]  # 要访问的URL
+def craw_url(url, img_dir='./output/img/'):
+    def process_img_url(url_of_img: str) -> str:
+        symbols = {'&', '!', '?'}
+        for idx, char in enumerate(url_of_img):
+            if char in symbols:
+                url_of_img = url_of_img[:idx]
+                break
+        url_of_img = ('https:' if url_of_img.startswith('//') else '') + url_of_img
+        return url_of_img
 
-    def get_urls(self, k=1.2):  # urls中可能存在无效网址
-        print(self.url2visit[0], 1)
-        for url in self.url2visit:  # 将要访问的URL队列
-            try:
-                f = request.urlopen(request.Request(url, headers=self.headers))
-            except:
-                continue
-            else:
-                bs = BeautifulSoup(f.read(), 'html.parser', from_encoding='gb18030')
-                if len(self.url2visit) >= k * self.url_num:
-                    break
-                for href in filter(None, map(lambda item: item.get('href'), bs.find_all('a'))):  # 获取所有a标签中的href的值
-                    if href.startswith('https://') and str(href).split('/')[2] in self.legal_urls:
-                        href = parse.quote(href, safe=string.printable)  # 中文字符需要处理
-                        if href not in self.url2visit:
-                            self.url2visit.append(href)
-                            print(href, len(self.url2visit))
+    global url_num, url_with_file, craw_res
+    try:
+        f = request.urlopen(request.Request(url, headers=headers))
+    except:
+        return False
+    else:
+        bs = BeautifulSoup(f.read(), 'html.parser', from_encoding='gb18030')
 
-    def craw(self):
-        url_with_file, out_lst, img_dir, all_files = 0, [], './output/img/', set()  # 带附件URL数目，输出的json列表
-        for url in self.url2visit:  # 将要访问的URL队列
-            try:
-                f = request.urlopen(request.Request(url, headers=self.headers))
-            except:
-                continue
-            else:
-                bs = BeautifulSoup(f.read(), 'html.parser', from_encoding='gb18030')
-                if len(out_lst) == self.url_num and url_with_file >= self.url_with_file:
-                    return out_lst
-
-                # 信息处理：获取标题、正文、附件
-                title_tag = bs.find('body').find('h1')
-                if title_tag and title_tag.getText().strip():
-                    title = title_tag.getText().strip()  # 新闻标题
-                    para = filter(None, [tag.text.replace('\n', '').replace(' ', '') for tag in bs.select('p')])  # 正文
-                    files, flag = set(), False  # flag标志是否已经标识为有照片
-                    for tag in bs.body.find_all('img'):  # 照片
-                        if 'src' in tag.attrs or 'data-original' in tag.attrs:  # 图片资源不全在src属性里，也在data-original中
-                            img_url = process_img_url(tag.attrs['src' if 'src' in tag.attrs else 'data-original'])
-                            file_name = img_url.split('/')[-1]  # 保存的文件名
-                            file_path = img_dir + file_name  # 文件相对地址
+        # 信息处理：获取标题、正文、附件
+        title_tag = bs.find('body').find('h1')
+        if title_tag and title_tag.getText().strip():
+            title = title_tag.getText().strip()  # 新闻标题
+            para = filter(None, [tag.text.replace('\n', '').replace(' ', '') for tag in bs.select('p')])  # 正文
+            files, flag = set(), False  # flag标志是否已经标识为有照片
+            for tag in bs.body.find_all('img'):  # 照片
+                if 'src' in tag.attrs or 'data-original' in tag.attrs:  # 图片资源不全在src属性里，也在data-original中
+                    img_url = process_img_url(tag.attrs['src' if 'src' in tag.attrs else 'data-original'])
+                    file_name = img_url.split('/')[-1]  # 保存的文件名
+                    file_path = img_dir + file_name  # 文件相对地址
+                    if file_name not in files:
+                        try:
+                            request.urlretrieve(img_url, file_path)  # 下载图片到file_path中
+                        except:
+                            continue
+                        else:
+                            flag = True
                             files.add(file_name)
-                            if file_name not in all_files:
-                                try:
-                                    request.urlretrieve(img_url, file_path)  # 下载图片到file_path中
-                                except:
-                                    continue
-                                else:
-                                    url_with_file, flag = url_with_file + (0 if flag else 1), True
-                                    all_files.add(file_name)
-                    out_lst.append({'url': url, 'title': title, 'paragraphs': ' '.join(para), 'file_name': list(files)})
-                    print(out_lst[-1])
-
-    def output_res(self, craw_res: list) -> list:  # 以json格式输出爬取的结果到文件中
-        res = [json.dumps(item, ensure_ascii=False) for item in craw_res]  # 构造输出的json格式列表
-        with open(self.craw_res_file, 'w', encoding='utf-8') as f:
-            f.write('\n'.join(res))
-        return res
+            lock.acquire()
+            if url_num >= num and url_with_file >= file_url_num:
+                lock.release()
+                return
+            url_num += 1
+            if flag:
+                url_with_file += 1
+            craw_res.append({'url': url, 'title': title, 'paragraphs': ' '.join(para), 'file_name': list(files)})
+            print('进度：%.2f' % str(float(url_num) / num * 100) + '%')
+            lock.release()
 
 
-def process_img_url(img_url: str) -> str:
-    """得到真正的html中获取的img资源地址.
+def output(craw_file='./output/craw_res.json') -> list:  # 以json格式输出爬取的结果到文件中
+    res = [json.dumps(item, ensure_ascii=False) for item in craw_res]  # 构造输出的json格式列表
+    with open(craw_file, 'w', encoding='utf-8') as f:
+        f.write('\n'.join(res))
+    return res
 
-    Args:
-        img_url: 通过beautifulSoup直接得到的图片资源地址.
 
-    Returns:
-        可以直接访问的，作为urllib.request.urlretrieve参数的URL.
-    """
+class MyThread(Thread):
+    def run(self) -> None:
+        while True:
+            try:
+                lock.acquire()
+                url = url_queue.get_nowait()
+                if url_num >= num and url_with_file >= file_url_num:
+                    lock.release()
+                    break
+                lock.release()
+                craw_url(url)
+            except:
+                break
 
-    # 获取处理前的图片URL地址
-    symbols = {'&', '!', '?'}
-    for idx, char in enumerate(img_url):
-        if char in symbols:
-            img_url = img_url[:idx]
-            break
 
-    # 处理URL地址以//开头
-    img_url = ('https:' if img_url.startswith('//') else '') + img_url
-    return img_url
+def main(thread_num=10):
+    # 抓取网页URL
+    print('*' * 100 + '\n开始获取网页地址...')
+    time0 = time.time()
+    for url in get_urls():
+        url_queue.put(url)
+    time1 = time.time()
+    print('获取完毕，用时：' + str(time1 - time0) + '秒\n' + '*' * 100 + '\n开始处理网页内容...')
+
+    # 爬取处理网页内容
+    threads = [MyThread(name='Thread' + str(idx)) for idx in range(thread_num)]
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+    time2 = time.time()
+    print('处理完成，用时：' + str(time2 - time1), '秒\n' + '*' * 100 + '\n开始导出结果...')
+
+    output()
+    print('导出完成。\n总用时%d秒。' % (time.time() - time0))
 
 
 if __name__ == '__main__':
-    craw = Craw()
-    print('*' * 100 + '\n开始获取网页地址...')
-    time0 = time.time()
-    craw.get_urls()
-    time1 = time.time()
-    print('获取完毕，用时：' + str(time1 - time0) + '秒\n' + '*' * 100 + '\n开始处理网页内容...')
-    craw_lst = craw.craw()
-    time2 = time.time()
-    print('处理完成，用时：' + str(time2 - time1), '秒\n' + '*' * 100 + '\n开始导出结果...')
-    craw.output_res(craw_lst)
-    print('导出完成')
+    main()
--- lab1/segment.py
+++ lab1/segment.py
@@ -8,43 +8,33 @@ import json
 import time
 
 
-class Segment:
-    def __init__(self, config_path='./input/config.json'):
-        with open(config_path, encoding='utf-8') as f:  # 读取配置文件
-            config_dic = json.load(f)
-            self.craw_res_file = config_dic['craw_res_file']  # 爬取的json结果文件
-            self.seg_res_file = config_dic['seg_res_file']  # 经分词的的结果文件
-            self.output_file = config_dic['output_file']  # 输出的结果文件
-            self.output_num = config_dic['output_num']  # 输出的json结果行数
-            self.cws_model_path = config_dic['cws_model_path']  # 分词模型路径，模型名称为`cws.model`
-            self.stop_words = config_dic['stop_words']  # 停用词文件
+def process(stop_words='stopwords.txt', craw_file='./output/craw_res.json',
+            model_path='E:/pyltp/ltp_data_v3.4.0/cws.model'):
+    with open(stop_words, 'r', encoding='utf-8') as f, open(craw_file, 'r', encoding='utf-8') as f1:
+        stop_words = set(f.read().split('\n'))  # 获取停用词
 
-    def process_craw_res(self):  # 将爬取结果分词并去除停用词
-        with open(self.stop_words, 'r', encoding='utf-8') as f, open(self.craw_res_file, 'r', encoding='utf-8') as f1:
-            stop_words = set(f.read().split('\n'))  # 获取停用词
-
-            from pyltp import Segmentor
-            seg, res = Segmentor(), []  # 初始化分词实例
-            seg.load(self.cws_model_path)  # 加载模型
-            for craw in [json.loads(line) for line in f1]:  # 按行转换json格式到python数据结构格式
-                title_lst = [word for word in seg.segment(craw['title']) if word not in stop_words]
-                para_lst = [word for word in seg.segment(craw['paragraphs']) if word not in stop_words]
-                res.append({'url': craw['url'], 'segmented_title': title_lst, 'segmented_paragraphs': para_lst,
-                            'file_name': craw['file_name']})
-            seg.release()
-            return res
-
-    def output_res(self, seg_lst: list) -> list:  # 以json格式输出处理的结果到文件中
-        res = [json.dumps(item, ensure_ascii=False) for item in seg_lst]  # 构造输出的json格式列表
-        with open(self.seg_res_file, 'w', encoding='utf-8') as f, open(self.output_file, 'w', encoding='utf-8') as f1:
-            f.write('\n'.join(res))  # 输出爬取结果的所有行
-            f1.write('\n'.join(res[:self.output_num]))  # 输出爬取结果的前几行
+        from pyltp import Segmentor
+        seg, res = Segmentor(), []  # 初始化分词实例
+        seg.load(model_path)  # 加载模型
+        for craw in [json.loads(line) for line in f1]:  # 按行转换json格式到python数据结构格式
+            title_lst = [word for word in seg.segment(craw['title']) if word not in stop_words]
+            para_lst = [word for word in seg.segment(craw['paragraphs']) if word not in stop_words]
+            res.append({'url': craw['url'], 'segmented_title': title_lst, 'segmented_paragraphs': para_lst,
+                        'file_name': craw['file_name']})
+        seg.release()
         return res
 
 
+def output(seg_lst: list, output_num=10, seg_file='./output/seg_res.json', output_file='./output/preprocessed.json'):
+    res = [json.dumps(item, ensure_ascii=False) for item in seg_lst]  # 构造输出的json格式列表
+    with open(seg_file, 'w', encoding='utf-8') as f, open(output_file, 'w', encoding='utf-8') as f1:
+        f.write('\n'.join(res))  # 输出爬取结果的所有行
+        f1.write('\n'.join(res[:output_num]))  # 输出爬取结果的前几行
+    return res
+
+
 if __name__ == '__main__':
-    segment = Segment()
     print('*' * 100 + '\n开始进行分词和停用词处理...')
     time0 = time.time()
-    segment.output_res(segment.process_craw_res())  # 将分词结果输出到配置文件中规定的seg_res_file和output_file文件中
+    output(process())  # 将分词结果输出到配置文件中规定的seg_res_file和output_file文件中
     print('处理完成，用时：' + str(time.time() - time0) + '秒\n' + '*' * 100)
