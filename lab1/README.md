4629846f4fbd7425e55c506f1b8bfee19d18d980
1172510217 <2584363094@qq.com>
2020-04-29 19:26:04 +0800

    :sparkles:礼貌策略的实现

--- lab1/README.md
+++ lab1/README.md
@@ -1,1 +1,23 @@
 # 实验一：网页文本的预处理
+## 环境
+- Python 3.6
+- Pycharm 专业版 2020.1
+- pyltp 分词工具
+- urllib 爬虫工具
+- bs4.BeautifuleSoup 解析工具
+## 输入输出（输出文件除了preprocessed.json外均未在code文件夹内）
+- 输入：停用词文件[stop_word.txt](./stopwords.txt)
+- 输入：分词模型文件cws.model，该文件需要下载
+- 输出：图片文件，code/img
+- 输出：craw_res.json，craw.py文件的默认产出文件，是爬取结果文件
+- 输出：seg_res.json，segment.py文件的默认产出文件，是分词和停用词处理结果
+- 输出：[preprocessed.json](./output/preprocessed.json)，seg_res.json文件的前10行
+## 如何运行
+- 运行[craw.py](./craw.py)文件：获取海量URL，并进行爬取处理，下载图片附件
+- 运行[segment.py](segment.py)文件：将craw.py文件的输出进行分词和停用词处理，并输出结果
+## 特点
+- 礼貌规则的处理
+  - 使用user-agent标识自己
+  - 使用urllib.robotparser进行机器人排除协议解析
+  - 降低带宽使用量，设置craw-delay参数
+- 多线程爬取网页：实验得出，爬取1000个网页耗时1h（单线程）->10分钟（多线程）
--- lab1/craw.py
+++ lab1/craw.py
@@ -4,40 +4,40 @@
 """
 爬取网页，并提取网页标题和正文，并保存附件（这里主要处理图片资源）.
 """
-from urllib import request, parse
+from urllib import request, parse, robotparser, error
 from bs4 import BeautifulSoup
 from threading import Thread, Lock
-from queue import Queue
+from queue import Queue, Empty
 import string
 import json
 import time
 
 lock = Lock()
-headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                         'Chrome/81.0.4044.122 Safari/537.36 Edg/81.0.416.64'}
 start_url, legal_urls = 'https://news.163.com/rank/', ['news.163.com', 'ent.163.com', 'gov.163.com', 'tech.163.com']
-url_queue, craw_res = Queue(), []
+user_agent = 'python3.6-urllib HIT-IR-lab1 author-zjr-1172510217'
+url_queue, craw_res, all_files = Queue(), [], set()  # 程序运行中，已经下载过的资源文件
 url_num, url_with_file = 0, 0  # 动态变量，随已爬取网页变化
 num, file_url_num = 1000, 100  # 定值，实验要求参数
+rps, craw_delay = [], 0  # 礼貌规则集合，爬取时间间隔
 
 
 def get_urls(k=1.2):
     url2visit = [start_url]  # 将要访问的URL队列
     for url in url2visit:
-        print('进度：%.2f' % str(float(len(url2visit)) / (num * 1.2) * 100) + '%')
+        print('抓取进度：%.2f%%' % (float(len(url2visit)) / (num * 1.2) * 100))
         try:
-            f = request.urlopen(request.Request(url, headers=headers))
-        except:
+            f = request.urlopen(request.Request(url, headers={'user-agent': 'IR-robot'}))
+        except error.URLError:
             continue
         else:
             bs = BeautifulSoup(f.read(), 'html.parser', from_encoding='gb18030')
             if len(url2visit) >= k * num:
                 return url2visit
             for href in filter(None, map(lambda item: item.get('href'), bs.find_all('a'))):  # 获取所有a标签中的href的值
-                if href.startswith('https://') and str(href).split('/')[2] in legal_urls:
+                if href.startswith('https://') and href.split('/')[2] in legal_urls:
                     href = parse.quote(href, safe=string.printable)  # 中文字符需要处理
-                    if href not in url2visit:
-                        url2visit.append(href)
+                    if href not in url2visit and rps[legal_urls.index(href.split('/')[2])].can_fetch('*', href):
+                        url2visit.append(href)  # 不违反机器人排除协议且未曾出现过，则将其加入到待访问URL队列中
 
 
 def craw_url(url, img_dir='./output/img/'):
@@ -52,8 +52,8 @@ def craw_url(url, img_dir='./output/img/'):
 
     global url_num, url_with_file, craw_res
     try:
-        f = request.urlopen(request.Request(url, headers=headers))
-    except:
+        f = request.urlopen(request.Request(url))
+    except error.URLError:
         return False
     else:
         bs = BeautifulSoup(f.read(), 'html.parser', from_encoding='gb18030')
@@ -70,10 +70,18 @@ def craw_url(url, img_dir='./output/img/'):
                     file_name = img_url.split('/')[-1]  # 保存的文件名
                     file_path = img_dir + file_name  # 文件相对地址
                     if file_name not in files:
-                        try:
-                            request.urlretrieve(img_url, file_path)  # 下载图片到file_path中
-                        except:
-                            continue
+                        visited = file_name in all_files  # 该图片是否被下载过
+                        if not visited:
+                            try:
+                                request.urlretrieve(img_url, file_path)  # 下载图片到file_path中
+                            except (error.URLError, TimeoutError):
+                                continue
+                            else:
+                                flag = True
+                                files.add(file_name)
+                                lock.acquire()
+                                all_files.add(file_name)
+                                lock.release()
                         else:
                             flag = True
                             files.add(file_name)
@@ -85,7 +93,7 @@ def craw_url(url, img_dir='./output/img/'):
             if flag:
                 url_with_file += 1
             craw_res.append({'url': url, 'title': title, 'paragraphs': ' '.join(para), 'file_name': list(files)})
-            print('进度：%.2f' % str(float(url_num) / num * 100) + '%')
+            print('处理进度：%.2f%%' % (float(url_num) / num * 100))
             lock.release()
 
 
@@ -105,15 +113,24 @@ class MyThread(Thread):
                 if url_num >= num and url_with_file >= file_url_num:
                     lock.release()
                     break
+                if craw_delay > 0:
+                    time.sleep(craw_delay)
                 lock.release()
                 craw_url(url)
-            except:
+            except (Empty, ConnectionResetError):
                 break
 
 
-def main(thread_num=10):
+def main(thread_num=15):
+    # 初始化所有的机器人排除协议
+    global rps
+    print('*' * 100 + '\n开始获取机器人排除协议信息...')
+    rps = [robotparser.RobotFileParser('https://' + legal_url + '/robots.txt') for legal_url in legal_urls]
+    for rp in rps:
+        rp.read()
+
     # 抓取网页URL
-    print('*' * 100 + '\n开始获取网页地址...')
+    print('获取完毕.\n' + '*' * 100 + '\n开始获取网页地址...')
     time0 = time.time()
     for url in get_urls():
         url_queue.put(url)
