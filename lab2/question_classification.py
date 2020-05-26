fb4d885de667015b60a6aa2c526de0e26dc24f43
1172510217 <2584363094@qq.com>
2020-05-26 15:02:42 +0800

    :sparkles:用逻辑回归模型进行问题分类

--- lab2/question_classification.py
+++ lab2/question_classification.py
@@ -1,86 +1,71 @@
 """
-运用机器学习方法进行问题多分类；
-机器学习方法：SVM.
+使用逻辑回归和SVM方法进行问题分类.
 """
-# todo 考虑是否去停用词
-# todo 0作为特征右侧填充
+from util import train_question_path, test_question_path, seg_line, lr_model_path, file_exists, tf_idf_path, read_json
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
-from util import seg_line, dump, load
-from sklearn.metrics import f1_score, recall_score, accuracy_score
-from sklearn.svm import SVC
-from os.path import exists
-import numpy as np
 import joblib
 
-word_model_path, svm_model_path = './question_classification/word', './question_classification/svm_model'
-train_path, test_path = './data/train_questions.txt', './data/test_questions.txt'
-labels, words, max_len = {}, {}, 0  # labels:{label: index}; words:{word: index}，其中word的index从1开始，因为0填充
 
-
-def pad_sequence(data: list, pad='post', truncating='pre', value=0):
-    res = []
-    for lst in data:
-        if len(lst) > max_len:
-            res.append(lst[-max_len:] if truncating == 'pre' else lst[:max_len])
-        else:
-            pad_lst = [value] * (max_len - len(lst))
-            res.append(lst + pad_lst if pad == 'post' else pad_lst + lst)
-    return res
-
-
-def test_init():  # 获取测试集文件
-    with open(test_path, 'r', encoding='utf-8') as f:
-        x_test, y_test = [], []
-        for line in f:
-            line = line.replace('\n', '')
-            if line:
-                [label, line] = line.split('\t')
-                x_test.append([words.get(word, 0) for word in seg_line(line)])
-                y_test.append(labels[label])
-        return pad_sequence(x_test), y_test
-
-
-def svm_init():
-    global words, labels, max_len
-    if exists(svm_model_path) and exists(word_model_path):  # 模型已经训练过，且存在对应的模型文件
-        json_dic = load(word_model_path)
-        words, labels, max_len = json_dic['words'], json_dic['labels'], json_dic['max_len']
-        clf = joblib.load(svm_model_path)
-    else:
-        with open(train_path, 'r', encoding='utf-8') as f:
-            x_train, y_train = [], []
+def load_data():  # 加载问题分类训练和测试数据
+    two_items = [(train_question_path, [], []), (test_question_path, [], [])]
+    for path, x_data, y_data in two_items:
+        with open(path, 'r', encoding='utf-8') as f:
             for line in f:
-                line = line.replace('\n', '')
-                if line:
-                    [label, line] = line.split('\t')
-                    labels[label] = labels.get(label, len(labels))
-                    x_train.append([])
-                    y_train.append(labels[label])
-                    for word in seg_line(line):
-                        words[word] = words.get(word, len(words) + 1)
-                        x_train[-1].append(words[word])
-            labels['OBJ_ADDRESS'] = len(labels)
-
-            max_len = max(len(item) for item in x_train)
-            dump(word_model_path, {'labels': labels, 'words': words, 'max_len': max_len})
-
-            svc = SVC(kernel='rbf', class_weight='balanced')
-            svc.fit(pad_sequence(x_train), y_train)
-            c_range = np.logspace(-5, 15, 11, base=2)
-            gamma_range = np.logspace(-9, 3, 13, base=2)
-            param_grid = [{'kernel': ['rbf'], 'C': c_range, 'gamma': gamma_range}]
-            grid = GridSearchCV(svc, param_grid, cv=3, n_jobs=-1)
-            clf = grid.fit(pad_sequence(x_train), y_train)
-            joblib.dump(clf, svm_model_path, compress=3)  # 导出模型
-    return clf
+                if len(line) > 1:
+                    [label, line] = line.strip().split('\t')
+                    x_data.append(' '.join(seg_line(line)))
+                    y_data.append(label)
+    return two_items[0][1], two_items[0][2], two_items[1][1], two_items[1][2]
 
 
-def main():
-    svc = svm_init()  # 加载模型
-    x_test, y_test = test_init()  # 获取测试集
-    res = svc.predict(x_test)
-    print(f1_score(y_test, res, average='macro'))
+def tf_idf_init(x_train):
+    if file_exists(tf_idf_path):
+        print('*' * 100, '\n正在加载VSM模型...')
+        return joblib.load(tf_idf_path)
+    else:
+        tf_idf_vec = TfidfVectorizer(token_pattern=r"(?u)\b\w+\b")
+        tf_idf_vec.fit_transform(x_train)
+        joblib.dump(tf_idf_vec, tf_idf_path)
+        return tf_idf_vec
+
+
+def lr_init(x_train, y_train):  # solver选用默认的lbfgs, multi_class选用多分类问题中的multinomial
+    if file_exists(lr_model_path):
+        print('*' * 100, '\n正在加载逻辑回归LR模型...')
+        return joblib.load(lr_model_path)
+    else:
+        print('*' * 100, '\n正在通过网格搜索获取最佳模型参数...')
+        lr = LogisticRegression(max_iter=400, n_jobs=-1)
+        param_grid = [{'C': [1, 5, 10, 50, 100, 500, 1000, 5000, 10000, 50000, 100000]}]
+        grid_search = GridSearchCV(lr, param_grid, cv=3, n_jobs=-1).fit(x_train, y_train)
+        joblib.dump(grid_search.best_estimator_, lr_model_path, compress=3)  # 导出模型
+        print('合理的超参数为', grid_search.best_params_)
+        return grid_search.best_estimator_
+
+
+def lr_predict():
+    x_train, y_train, x_test, y_test = load_data()
+    tf_idf_vec = tf_idf_init(x_train)
+    x_train, x_test = tf_idf_vec.transform(x_train), tf_idf_vec.transform(x_test)
+    lr = lr_init(x_train, y_train)
+
+    json_lst, x_data = read_json('./preprocessed/train_preprocessed.json'), []
+    for item in json_lst:
+        x_data.append(' '.join(item['question']))
+    y_data = lr.predict(tf_idf_vec.transform(x_data))
+    return y_data
+
+
+def lr_model_init():
+    x_train, y_train, x_test, y_test = load_data()
+    tf_idf_vec = tf_idf_init(x_train)
+    x_train, x_test = tf_idf_vec.transform(x_train), tf_idf_vec.transform(x_test)
+    lr = lr_init(x_train, y_train)
+    print('模型准确率：%.4f%%' % (lr.score(x_test, y_test) * 100))
 
 
 if __name__ == '__main__':
-    main()
+    lr_model_init()  # 训练模型或者加载模型
+    # res = lr_predict()  # 预测问题分类
